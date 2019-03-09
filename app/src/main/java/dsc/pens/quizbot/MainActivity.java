6f19129f3624ab069583c188192e8e0129237042
Ade Guntur Ramadhan <ade_guntur_26rpl@student.smktelkom-mlg.sch.id>
2019-03-09 16:34:25 +0700

    Initial commit

---
+++ app/src/main/java/dsc/pens/quizbot/MainActivity.java
@@ -0,0 +1,139 @@
+package dsc.pens.quizbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+import ai.api.AIServiceContext;
+import ai.api.AIServiceContextBuilder;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int USER = 10001;
+    private static final int BOT = 10002;
+
+    private String uuid = UUID.randomUUID().toString();
+    private LinearLayout chatLayout;
+    private EditText queryEditText;
+
+    // Android client
+    private AIRequest aiRequest;
+    private AIDataService aiDataService;
+    private AIServiceContext customAIServiceContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ScrollView scrollview = findViewById(R.id.chatScrollView);
+        scrollview.post(() -> scrollview.fullScroll(ScrollView.FOCUS_DOWN));
+
+        chatLayout = findViewById(R.id.chatLayout);
+
+        ImageView sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(this::sendMessage);
+
+        queryEditText = findViewById(R.id.queryEditText);
+        queryEditText.setOnKeyListener((view, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_DPAD_CENTER:
+                    case KeyEvent.KEYCODE_ENTER:
+                        sendMessage(sendBtn);
+                        return true;
+                    default:
+                        break;
+                }
+            }
+            return false;
+        });
+
+        initChatbot();
+
+    }
+
+    private void initChatbot() {
+        final AIConfiguration config = new AIConfiguration(BuildConfig.ClientAccessToken,
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiDataService = new AIDataService(this, config);
+        customAIServiceContext = AIServiceContextBuilder.buildFromSessionId(uuid);// helps to create new session whenever app restarts
+        aiRequest = new AIRequest();
+    }
+
+    private void sendMessage(View view) {
+        String msg = queryEditText.getText().toString();
+        if (msg.trim().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please enter your query!", Toast.LENGTH_LONG).show();
+        } else {
+            showTextView(msg, USER);
+            queryEditText.setText("");
+            aiRequest.setQuery(msg);
+            RequestTask requestTask = new RequestTask(MainActivity.this, aiDataService, customAIServiceContext);
+            requestTask.execute(aiRequest);
+        }
+    }
+
+    public void callback(AIResponse aiResponse) {
+        if (aiResponse != null) {
+            // process aiResponse here
+            String botReply = aiResponse.getResult().getFulfillment().getSpeech();
+            Log.d(TAG, "Bot Reply: " + botReply);
+            showTextView(botReply, BOT);
+        } else {
+            Log.d(TAG, "Bot Reply: Null");
+            showTextView("There was some communication issue. Please Try again!", BOT);
+        }
+    }
+    private void showTextView(String message, int type) {
+        FrameLayout layout;
+        switch (type) {
+            case USER:
+                layout = getUserLayout();
+                break;
+            case BOT:
+                layout = getBotLayout();
+                break;
+            default:
+                layout = getBotLayout();
+                break;
+        }
+        layout.setFocusableInTouchMode(true);
+        chatLayout.addView(layout); // move focus to text view to automatically make it scroll up if softfocus
+        TextView tv = layout.findViewById(R.id.chatMsg);
+        tv.setText(message);
+        layout.requestFocus();
+        queryEditText.requestFocus(); // change focus back to edit text to continue typing
+    }
+
+    FrameLayout getUserLayout() {
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        return (FrameLayout) inflater.inflate(R.layout.user_msg_layout, null);
+    }
+
+    FrameLayout getBotLayout() {
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        return (FrameLayout) inflater.inflate(R.layout.bot_msg_layout, null);
+    }
+}
+
