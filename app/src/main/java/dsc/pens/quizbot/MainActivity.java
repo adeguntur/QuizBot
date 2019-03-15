70cd6dbfdb679db4328a078a86cc95bc8f2dfa6c
affand20 <fahri.lafa@gmail.com>
2019-03-15 13:38:24 +0700

    add speech recognition

--- app/src/main/java/dsc/pens/quizbot/MainActivity.java
+++ app/src/main/java/dsc/pens/quizbot/MainActivity.java
@@ -1,10 +1,19 @@
 package dsc.pens.quizbot;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -15,6 +24,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.UUID;
 
 import ai.api.AIServiceContext;
@@ -33,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private String uuid = UUID.randomUUID().toString();
     private LinearLayout chatLayout;
     private EditText queryEditText;
+    private TextView textMessage;
 
     // Android client
     private AIRequest aiRequest;
@@ -44,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO
+        )!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},200);
+        }
 
         final ScrollView scrollview = findViewById(R.id.chatScrollView);
         scrollview.post(() -> scrollview.fullScroll(ScrollView.FOCUS_DOWN));
@@ -67,6 +84,85 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        ImageView voiceBtn = findViewById(R.id.voiceBtn);
+
+        SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "in_ID");
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+                mSpeechRecognizer.stopListening();
+                textMessage.setText("Silakan coba lagi :(");
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> speechresult = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                Log.d(TAG, "onResults: "+speechresult.size());
+                textMessage.setText(speechresult.get(0));
+                sendMessage(speechresult.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                ArrayList<String> speechresult = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                Log.d(TAG, "onPartialResults: "+speechresult.get(0));
+                if (!speechresult.get(0).equals("")) {
+                    textMessage.setText(speechresult.get(0));
+                }
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+        voiceBtn.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                showTextView("Mendengarkan...", USER);
+                return true;
+            }
+            if (motionEvent.getAction()==MotionEvent.ACTION_UP){
+                mSpeechRecognizer.stopListening();
+            }
+            return true;
+        });
+
 
         initChatbot();
 
@@ -92,6 +188,12 @@ public class MainActivity extends AppCompatActivity {
             RequestTask requestTask = new RequestTask(MainActivity.this, aiDataService, customAIServiceContext);
             requestTask.execute(aiRequest);
         }
+    }
+
+    private void sendMessage(String msg){
+        aiRequest.setQuery(msg);
+        RequestTask requestTask = new RequestTask(MainActivity.this, aiDataService, customAIServiceContext);
+        requestTask.execute(aiRequest);
     }
 
     public void callback(AIResponse aiResponse) {
@@ -121,6 +223,7 @@ public class MainActivity extends AppCompatActivity {
         layout.setFocusableInTouchMode(true);
         chatLayout.addView(layout); // move focus to text view to automatically make it scroll up if softfocus
         TextView tv = layout.findViewById(R.id.chatMsg);
+        textMessage = tv;
         tv.setText(message);
         layout.requestFocus();
         queryEditText.requestFocus(); // change focus back to edit text to continue typing
